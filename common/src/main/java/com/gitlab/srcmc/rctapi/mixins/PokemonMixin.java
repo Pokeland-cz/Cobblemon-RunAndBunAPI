571d894f29e9977271a45554c5c4b1f6ef29fae9
hdainester <27729552+hdainester@users.noreply.github.com>
2025-01-21 18:40:15 +0100

    imort format

--- common/src/main/java/com/gitlab/srcmc/rctapi/mixins/PokemonMixin.java
+++ common/src/main/java/com/gitlab/srcmc/rctapi/mixins/PokemonMixin.java
@@ -23,7 +23,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.cobblemon.mod.common.pokemon.Pokemon;
-
 import com.gitlab.srcmc.rctapi.api.RCTApi;
 import com.gitlab.srcmc.rctapi.api.trainer.Trainer;
 import net.minecraft.world.entity.LivingEntity;
